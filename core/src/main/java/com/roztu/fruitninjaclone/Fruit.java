88e9602c56f48f49cb24e5a8ca7c0e9e4465de7f
Tuğçe Özturaç <t.ozturac@gmail.com>
2025-09-02 14:59:10 +0300

    first commit

--- core/src/main/java/com/roztu/fruitninjaclone/Fruit.java
+++ core/src/main/java/com/roztu/fruitninjaclone/Fruit.java
@@ -4,40 +4,45 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
 public class Fruit {
-    public static float radius = 60f ;
-    public enum Type{
-        REGULAR,EXTRA,BOMB,LIFE
+    public static float radius = 60f;
+
+    public enum Type {
+        REGULAR, EXTRA, BOMB, LIFE
     }
-    Type type;
-    Fruit(Vector2 pos, Vector2 velocity){
+
+    private static final float GRAVITY = 0.2f;   // yerçekimi katsayısı
+    private static final float FRICTION = 5f;    // yatay yavaşlama
+
+    public Type type;
+    private Vector2 pos, velocity;
+    public boolean living = true;
+
+    // EXTRA meyveler için id
+    public int extraId = -1;
+
+    public Fruit(Vector2 pos, Vector2 velocity) {
         this.pos = pos;
         this.velocity = velocity;
-        type = Type.REGULAR;
+        this.type = Type.REGULAR;
     }
-    Vector2 pos, velocity;
-    public boolean living = true;
-    public boolean clicked(Vector2 click){
-        if(pos.dst2(click)<=radius * radius + 1) return true;
-        return false;
+
+    public boolean clicked(Vector2 click) {
+        return pos.dst2(click) <= radius * radius;
     }
-    public void update(float dt){
-        velocity.y-=dt*(Gdx.graphics.getHeight()*0.2f);
-        //velocity.x-=dt*(Gdx.graphics.getWidth()*0.2f);
-        velocity.x-=dt*Math.signum(velocity.x) * 5f;
-        //The java.lang.Math.signum(float f) returns the signum function of the argument; zero if the argument is zero, 1.0f if the argument is greater than zero, -1.0f if the argument is less than zero.Special Cases −
+
+    public void update(float dt) {
+        // Yerçekimi ve sürtünme etkisi
+        velocity.y -= dt * (Gdx.graphics.getHeight() * GRAVITY);
+        velocity.x -= dt * Math.signum(velocity.x) * FRICTION;
+
         pos.mulAdd(velocity, dt);
     }
 
-    //         public boolean outOfScreen(){
-    //            return (pos.y<0);
-    //        }
-    public boolean outOfScreen(){
-        return (pos.y<-2f*radius);
+    public boolean outOfScreen() {
+        return (pos.y < -2f * radius || pos.x < -2f * radius || pos.x > Gdx.graphics.getWidth() + 2f * radius);
     }
 
-    public final Vector2 getPos(){
+    public Vector2 getPos() {
         return pos;
     }
-
-
 }
--- core/src/main/java/com/roztu/fruitninjaclone/Main.java
+++ core/src/main/java/com/roztu/fruitninjaclone/Main.java
@@ -4,53 +4,40 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
 import java.util.Random;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter implements InputProcessor {
     private SpriteBatch batch;
     private Texture background;
-    Texture banana;
-    Texture greengrepe;
-    Texture apple ;
-    Texture lemon ;
-    Texture bomb;
-    Texture coins;
-    BitmapFont font;
-    FreeTypeFontGenerator fontGen ;
-    Random random = new Random();
-    Array<Fruit>fruitArray =new Array<Fruit>();
+    private Texture banana, greengrepe, apple, lemon, bomb, coins, strawberry , blackCherry ;
+    private BitmapFont font;
+    private FreeTypeFontGenerator fontGen;
+    private Random random = new Random();
 
+    private Array<Fruit> fruitArray = new Array<>();
 
-    float genCounter = 0;
+    private float genCounter = 0;
     private final float startGenSpeed = 1.1f;
-    float genSpeed = startGenSpeed;
+    private float genSpeed = startGenSpeed;
 
-
-    int lives = 4;
-    int score = 0;
-    private double currentTime;
-    private double gameOverTime = -1.0f;
-
-
-
-
-
-
+    private int lives = 4;
+    private int score = 0;
+    private double gameOverTime = -1.0;
+    private double startTime;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+        // görseller
         background = new Texture("backround.jpg");
         banana = new Texture("banana.png");
         greengrepe = new Texture("green-grape.png");
@@ -58,56 +45,54 @@ public class Main extends ApplicationAdapter implements InputProcessor {
         lemon = new Texture("lemon.png");
         bomb = new Texture("bomb.png");
         coins = new Texture("coins.png");
+        blackCherry = new Texture("black-cherry.png");
+        strawberry = new Texture("strawberry.png");
 
-        Fruit.radius = Math.max(Gdx.graphics.getHeight(),Gdx.graphics.getWidth())/20f;
+        Fruit.radius = Math.max(Gdx.graphics.getHeight(), Gdx.graphics.getWidth()) / 20f;
         Gdx.input.setInputProcessor(this);
 
+        // font
         fontGen = new FreeTypeFontGenerator(Gdx.files.internal("robotobold.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        params.color = Color.WHITE ;
+        params.color = Color.WHITE;
         params.size = 40;
         params.characters = "0123456789 CutoplayScre : .+-";
         font = fontGen.generateFont(params);
+
+        // başlangıç zamanı
+        startTime = TimeUtils.millis() / 1000.0;
     }
 
     @Override
     public void render() {
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        float elapsedTime = (float) ((TimeUtils.millis() / 1000.0) - startTime);
 
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        double newTime = TimeUtils.millis() / 1000.0;
-        System.out.println("newtime: " + newTime);
-        double frameTime = Math.min(newTime - currentTime, 0.3);
-        System.out.println("frametime: " + frameTime);
-        float deltaTime = (float) frameTime;
-        System.out.println("deltatime: " + deltaTime);
-        currentTime = newTime;
-
-        if (lives <= 0 && gameOverTime == 0f) {
-            //end game
-            gameOverTime = currentTime;
+        if (lives <= 0 && gameOverTime == -1.0) {
+            gameOverTime = TimeUtils.millis() / 1000.0;
         }
 
         if (lives > 0) {
-            //game  mode
-
-            genSpeed -= deltaTime * 0.015f;
-
-            System.out.println("genspeed: " + genSpeed);
-            System.out.println("gencounter: " + genCounter);
+            // oyun modu
+            genSpeed = Math.max(0.5f, genSpeed - deltaTime * 0.015f);
 
             if (genCounter <= 0f) {
                 genCounter = genSpeed;
-                addItem();
+                addItem(elapsedTime);
             } else {
                 genCounter -= deltaTime;
             }
 
-
-            for (int i = 0; i < lives; i++) {
+            // canları ekrana çiz
+            for (int i = 0; i < Math.min(lives, 5); i++) {
                 batch.draw(apple, i * 30f + 20f, Gdx.graphics.getHeight() - 25f, 25f, 25f);
             }
+
+
+            // meyveleri güncelle ve çiz
             for (Fruit fruit : fruitArray) {
                 fruit.update(deltaTime);
                 switch (fruit.type) {
@@ -115,7 +100,13 @@ public class Main extends ApplicationAdapter implements InputProcessor {
                         batch.draw(apple, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius);
                         break;
                     case EXTRA:
-                        batch.draw(lemon, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius);
+                        switch (fruit.extraId) {
+                            case 0: batch.draw(lemon, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius); break;
+                            case 1: batch.draw(greengrepe, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius); break;
+                            case 2: batch.draw(strawberry, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius); break;
+                            case 3: batch.draw(blackCherry, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius); break;
+                            case 4: batch.draw(banana, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius); break;
+                        }
                         break;
                     case BOMB:
                         batch.draw(bomb, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius);
@@ -124,24 +115,21 @@ public class Main extends ApplicationAdapter implements InputProcessor {
                         batch.draw(coins, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius);
                         break;
                 }
-
             }
 
-            //remove out of screen
-            boolean holdlives = false;
-            Array<Fruit> toRemove = new Array<Fruit>();
+            // ekrandan çıkanları temizle
+            Array<Fruit> toRemove = new Array<>();
+            boolean lostLife = false;
             for (Fruit fruit : fruitArray) {
                 if (fruit.outOfScreen()) {
                     toRemove.add(fruit);
                     if (fruit.living && fruit.type == Fruit.Type.REGULAR) {
                         lives--;
-                        holdlives = true;
-                        //holdlives=true;
-                        break;
+                        lostLife = true;
                     }
                 }
             }
-            if (holdlives) {
+            if (lostLife) {
                 for (Fruit f : fruitArray) {
                     f.living = false;
                 }
@@ -149,31 +137,47 @@ public class Main extends ApplicationAdapter implements InputProcessor {
             for (Fruit f : toRemove) {
                 fruitArray.removeValue(f, true);
             }
-
-
         }
-        font.draw(batch,"Score: "+score,30,45);
-        if(lives <= 0){
-            font.draw(batch,"Cut to play",Gdx.graphics.getWidth()*0.5f,Gdx.graphics.getHeight()*0.5f);
+
+        // skor
+        font.draw(batch, "Score: " + score, 30, 45);
+
+        // game over yazısı
+        if (lives <= 0) {
+            font.getData().setScale(2f);
+            font.draw(batch, "Cut to Play!", Gdx.graphics.getWidth() * 0.35f, Gdx.graphics.getHeight() * 0.5f);
+            font.getData().setScale(1f);
         }
 
         batch.end();
     }
 
-    private void addItem(){
-        float pos = random.nextFloat()*Gdx.graphics.getWidth();
-       // Fruit item = new Fruit(new Vector2(pos,pos),new Vector2(pos,pos));
-        Fruit item = new Fruit(new Vector2(pos,-Fruit.radius),new Vector2((Gdx.graphics.getWidth()*0.5f - pos) * 0.3f+(random.nextFloat() - 0.5f),Gdx.graphics.getHeight()*0.5f));
-        float type = random.nextFloat();
-        if(type>0.96){
-            item.type = Fruit.Type.LIFE;
-        } else if(type>0.88){
-            item.type = Fruit.Type.EXTRA;
-        } else if(type>0.78){
-            item.type = Fruit.Type.BOMB;
-        }
-        fruitArray.add(item);
+    private void addItem(float elapsedTime) {
+        if (fruitArray.size >= 7) return; // max 7 meyve ekranda olsun
 
+        float pos = random.nextFloat() * Gdx.graphics.getWidth();
+        Fruit item = new Fruit(
+            new Vector2(pos, -Fruit.radius),
+            new Vector2((Gdx.graphics.getWidth() * 0.5f - pos) * 0.3f + (random.nextFloat() - 0.5f),
+                Gdx.graphics.getHeight() * 0.5f)
+        );
+
+        // Zorluk ilerledikçe bombaların ihtimali artsın
+        float difficulty = Math.min(1f, elapsedTime / 60f); // 1 dakikada max zorluk
+        float r = random.nextFloat();
+
+        if (r < 0.05f) {
+            item.type = Fruit.Type.LIFE;   // %5
+        } else if (r < 0.25f) {
+            item.type = Fruit.Type.EXTRA;  // %20
+            item.extraId = random.nextInt(5); // 0-4 arası ekstra meyve seç
+        } else if (r < 0.25f + (0.1f + 0.2f * difficulty)) {
+            item.type = Fruit.Type.BOMB;   // %10 → %30 arası
+        } else {
+            item.type = Fruit.Type.REGULAR; // geri kalan %45–65
+        }
+
+        fruitArray.add(item);
     }
 
     @Override
@@ -181,68 +185,40 @@ public class Main extends ApplicationAdapter implements InputProcessor {
         batch.dispose();
         font.dispose();
         fontGen.dispose();
-
+        background.dispose();
+        banana.dispose();
+        greengrepe.dispose();
+        apple.dispose();
+        lemon.dispose();
+        bomb.dispose();
+        coins.dispose();
+        strawberry.dispose();
+        blackCherry.dispose();
     }
 
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
+    // InputProcessor metodları
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        if(lives<=0 && currentTime - gameOverTime > 2f){//menu mode
-            gameOverTime = 0f;
-            score=0;
-            lives=4;//restart game
+        if (lives <= 0) {
+            gameOverTime = -1.0;
+            score = 0;
+            lives = 4;
             genSpeed = startGenSpeed;
             fruitArray.clear();
-        }else{
-
-            //game mode
-            Array<Fruit> toRemove = new Array<Fruit>();
-            Vector2 pos = new Vector2(screenX,Gdx.graphics.getHeight()-screenY);
+        } else {
+            Array<Fruit> toRemove = new Array<>();
+            Vector2 pos = new Vector2(screenX, Gdx.graphics.getHeight() - screenY);
             int plusScore = 0;
-            for(Fruit f : fruitArray){
-                System.out.println("getHeight - y: " + screenY);
-                System.out.println("getHeight - y: " + (Gdx.graphics.getHeight()-screenY));
-                System.out.println("getHeight - y: " + f.getPos());
-                System.out.println("distance: " + pos.dst2(f.pos));
-                System.out.println("distance: " + f.clicked(pos));
-                System.out.println("distance: " + Fruit.radius * Fruit.radius + 1);
-                if(f.clicked(pos)){
+
+            for (Fruit f : fruitArray) {
+                if (f.clicked(pos)) {
                     toRemove.add(f);
-                    switch(f.type){
+                    switch (f.type) {
                         case REGULAR:
                             plusScore++;
                             break;
                         case EXTRA:
-                            plusScore+=2;
+                            plusScore += 2;
                             score++;
                             break;
                         case BOMB:
@@ -250,27 +226,26 @@ public class Main extends ApplicationAdapter implements InputProcessor {
                             break;
                         case LIFE:
                             lives++;
+                            if (lives > 5) lives = 5; // maksimum 5 can
                             break;
+
                     }
                 }
             }
-            score += plusScore*plusScore;
-            for(Fruit f : toRemove){
+            score += plusScore * plusScore;
+            for (Fruit f : toRemove) {
                 fruitArray.removeValue(f, true);
             }
         }
         return false;
-
-
     }
 
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
-    }
+    @Override public boolean keyDown(int keycode) { return false; }
+    @Override public boolean keyUp(int keycode) { return false; }
+    @Override public boolean keyTyped(char character) { return false; }
+    @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) { return false; }
+    @Override public boolean touchUp(int screenX, int screenY, int pointer, int button) { return false; }
+    @Override public boolean touchCancelled(int screenX, int screenY, int pointer, int button) { return false; }
+    @Override public boolean mouseMoved(int screenX, int screenY) { return false; }
+    @Override public boolean scrolled(float amountX, float amountY) { return false; }
 }
